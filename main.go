e58bb3ed871cd22e0ebae97c0c7cc27214728c7f
Lovelesh Sharma <lovelesh90@gmail.com>
2023-04-15 22:23:19 +0530

    Adds main function to run the program

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	ch := NewConsistentHashing(1000)
+
+	ch.PlotServer("s1")
+	ch.PlotServer("s2")
+	ch.PlotServer("s3")
+
+	ch.PlotKey("k3")
+	ch.PlotKey("k2")
+	ch.PlotKey("k1")
+
+	sr1 := ch.GetServer("k1")
+	fmt.Println("server for k1: ", sr1.name)
+
+	sr2 := ch.GetServer("k2")
+	fmt.Println("server for k2: ", sr2.name)
+
+	sr3 := ch.GetServer("k3")
+	fmt.Println("server for k3: ", sr3.name)
 }
